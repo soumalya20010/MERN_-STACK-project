26790219eb4b75a869db7ae166832dc207b745c8
soumalya biswas <soumalyaabiswas2001@gmail.com>
2023-04-05 21:15:17 +0530

    1st

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,13 +11,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <Navbar/>
+       
         <title>Pizza Palace</title>
         <meta name="description" content="Best pizza in town" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="png" href="C:\Users\souma\Desktop\New folder\restaurant-app\public\pizza favi.png"/>
       </Head>
-      <Footer />
+     
  homepage 
  </div>
    
