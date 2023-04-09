1a257e6ce54918075b174d2a2f61bcbfd4610b30
soumalya biswas <soumalyaabiswas2001@gmail.com>
2023-04-09 19:32:46 +0530

    1st

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div className={styles.container}>
             <div className={styles.item}>
             <div className={styles.callButton}>
-            <Image src="/img/telephone.jpg" alt="" width="132" height="132"/>
+            <Image src="/img/telephone.png" alt="" width="30" height="30" />
 
                 </div>
 <div className={styles.texts}>
@@ -13,11 +13,27 @@ const Navbar = () => {
     <div className={styles.text}>012 345 678</div>
 </div>
 </div>
-                <div className={styles.item}></div>
-                <div className={styles.item}></div>
+
+                <div className={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>Homepage</li>
+                    <li className={styles.listItem}>Products</li>
+                    <li className={styles.listItem}>Menu</li>
+                    <Image src="/img/pizza_logo1.png" alt="" width="150" height="100" />
+                    <li className={styles.listItem}>Events</li>
+                    <li className={styles.listItem}>Blog</li>
+                    <li className={styles.listItem}>Contact</li>
+</ul>
+</div>
+                <div className={styles.item}>
+                <div className={styles.carts}>
+                    <Image src="/img/shopping_cart.png" alt="" width="35" height="30"/>
+                   <div>
+2
                     </div>
-          
-    
+          </div>
+          </div>
+    </div>
     );
 };
  
