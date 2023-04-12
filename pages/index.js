349095a0375ad88ad5f499a90349cc802bf4cf20
soumalya biswas <soumalyaabiswas2001@gmail.com>
2023-04-13 01:59:47 +0530

    e

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,41 @@
-import Head from 'next/head';
-import Image from 'next/legacy/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import Featured from '@/components/Featured';
-import PizzaList from '@/components/PizzaList';
+import axios from "axios";
+import Head from "next/head";
+import Image from "next/legacy/image";
+import { useState } from "react";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
+import styles from "../styles/Home.module.css";
 
-
-
-
-
-export default function Home() {
+export default function Home({ pizzaList, admin }) {
+  const [close, setClose] = useState(true);
   return (
     <div className={styles.container}>
       <Head>
-       
-        <title>Pizza Palace</title>
-        <meta name="description" content="Best pizza in town" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="C:\Users\souma\Desktop\New folder\restaurant-app\public\favicon.ico"/>
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-     
-<Featured/>
-<PizzaList/>
- </div>
-   
+      <Featured />
+      
+      <PizzaList pizzaList={pizzaList} />
+      {!close && <Add setClose={setClose} />}
+    </div>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
+
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      pizzaList: res.data,
+      admin,
+    },
+  };
+};
