78122ea1680375e039dd350287c2ff1b4cabbc17
Soumalya Biswas <soumalyaabiswas2001@gmail.com>
2025-02-08 03:43:20 +0530

    d

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -97,7 +97,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `https://mern-stack-project-plum.vercel.app/api/products${params.id}`
   );
   return {
     props: {
