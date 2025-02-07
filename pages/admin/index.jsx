55ac0353c7d9e96082c0c4692b07508c986c7400
Soumalya Biswas <soumalyaabiswas2001@gmail.com>
2025-02-08 02:29:32 +0530

    d

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -9,7 +9,7 @@ const AdminPage = ({ orders, products, admin }) => {
 
   const handleDelete = async (id) => {
     try {
-      const res = await axios.delete(`http://localhost:3000/api/products/${id}`);
+      const res = await axios.delete(`https://mern-stack-project-plum.vercel.app/api/products/${id}`);
       setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
     } catch (err) {
       console.error(err);
@@ -21,7 +21,7 @@ const AdminPage = ({ orders, products, admin }) => {
     const currentStatus = item.status;
 
     try {
-      const res = await axios.put(`http://localhost:3000/api/orders/${id}`, {
+      const res = await axios.put(`https://mern-stack-project-plum.vercel.app/api/orders/${id}`, {
         status: currentStatus + 1,
       });
       setOrderList(
@@ -101,8 +101,8 @@ export const getServerSideProps = async (ctx) => {
     };
   }
 
-  const resOrders = await axios.get("http://localhost:3000/api/orders");
-  const resProducts = await axios.get("http://localhost:3000/api/products");
+  const resOrders = await axios.get("https://mern-stack-project-plum.vercel.app/api/orders");
+  const resProducts = await axios.get("https://mern-stack-project-plum.vercel.app/api/products");
 
   return {
     props: {
