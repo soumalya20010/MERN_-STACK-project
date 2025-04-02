7426ecf066ca5128fe3027fb07d98512e3dab2f9
Soumalya Biswas <soumalyaabiswas2001@gmail.com>
2025-04-02 19:28:18 +0530

    UI update

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -1,9 +1,27 @@
 import styles from "../styles/PizzaList.module.css";
-import PizzaCard from "./PizzaCard"
+import PizzaCard from "./PizzaCard";
+import Image from "next/image";
 
 const PizzaList = ({ pizzaList,IsLoggedIn }) => {
+  
   return (
     <div className={styles.container}>
+      <div className={styles.images}> 
+       <Image  src="/img/shape_1.png" // Path to your image in the public folder
+          alt="Shape 1"
+          width={200} // Set the width of the image
+          height={200} // Set the height of the image
+          className={styles.imageRight} 
+        />
+        
+
+       <Image  src="/img/shape_2.png" // Path to another image
+          alt="Shape 2"
+          width={200}
+          height={200}
+          className={styles.imageLeft} 
+        />
+      </div>
       <h1 className={styles.title}>Pizza Available</h1>
       <p className={styles.desc}>
        
